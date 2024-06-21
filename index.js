8db33f187da997855686dd6404d567f8f70a731a
miguelwebcode <miguelwebcode@gmail.com>
2024-06-21 20:23:43 +0200

    Create get method for root endpoint

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ import axios from 'axios';
 const app = express();
 const PORT = 3000;
 const BASEURL = "https://rickandmortyapi.com/api/";
+let endpoints = {};
 
 app.use(bodyParser.json());
 
-// 3. Use the public folder for static files.
+//Use the public folder for static files.
 app.use(express.static("public"));
+
+
+//When the user goes to the home page it should render the index.ejs file.
+app.get("/", async () => {
+    let response = await axios.get(`${BASEURL}`);
+    //Store the endpoints in a global variable to be used in other routes.
+    endpoints = response.data;
+    res.render("index.ejs", {content: endpoints});
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
